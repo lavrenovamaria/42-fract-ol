bca9e47f4fd0ca6ca25543bd1f0d7ca5d9fa5772
lavrenovamaria <wrickard@student.21-school.ru>
2021-09-11 19:15:02 +0300

    colors

--- srcs/main.c
+++ srcs/main.c
@@ -1,7 +1,8 @@
 #include "../minilibx_opengl_20191021/mlx.h"
 #include <unistd.h>
 
-typedef struct	s_data {
+typedef struct	s_data
+{
 	void	*img;
 	char	*addr;
 	int		bits_per_pixel;
@@ -9,6 +10,7 @@ typedef struct	s_data {
 	int		endian;
 }				t_data;
 
+//original
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
 	char	*dst;
@@ -16,6 +18,7 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
 	*(unsigned int*)dst = color;
 }
+
 //основной мейник, выводящий ряд пикселей разных цветов
 // int	main(void)
 // {
@@ -26,7 +29,7 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 // 	// int count2 = 5;
 // 	int i = 0;
 // 	int idx = 0;
-// 	int color[3] = {0x00FF0000, 0x0000FF00, 0x000000FF};
+// 	int color[3] = {0x00FF0000, 0x00FF0000, 0x00FF0000};
 
 // 	mlx = mlx_init();
 // 	mlx_win = mlx_new_window(mlx, 500, 500, "Galochka, ti schas umresh");
@@ -39,43 +42,69 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 // 	mlx_loop(mlx);
 // }
 
-int main()
+// //-----BACKGROUND-----
+// int main()
+// {
+// 	void *mlx = mlx_init();
+// 	void *win = mlx_new_window(mlx, 640, 400, "Tutorial Window - Create Image");
+// 	void *image = mlx_new_image(mlx, 640, 400);
+// 	int pixel_bits;
+// 	int line_bytes;
+// 	int endian;
+// 	char *buffer = mlx_get_data_addr(image, &pixel_bits, &line_bytes, &endian);
+
+// 	int color = 0xABEFCD;
+
+// 	if (pixel_bits != 32)
+// 		color = mlx_get_color_value(mlx, color);
+
+// 	for(int y = 0; y < 400; ++y)
+// 	for(int x = 0; x < 640; ++x)
+// {
+// 	int pixel = (y * line_bytes) + (x * 4);
+
+// 	if (endian == 1)        // Most significant (Alpha) byte first
+// 	{
+// 		buffer[pixel + 0] = (color >> 24);
+// 		buffer[pixel + 1] = (color >> 16) & 0xFF;
+// 		buffer[pixel + 2] = (color >> 8) & 0xFF;
+// 		buffer[pixel + 3] = (color) & 0xFF;
+// 	}
+// 	else if (endian == 0)   // Least significant (Blue) byte first
+// 	{
+// 		buffer[pixel + 0] = (color) & 0xFF;
+// 		buffer[pixel + 1] = (color >> 8) & 0xFF;
+// 		buffer[pixel + 2] = (color >> 16) & 0xFF;
+// 		buffer[pixel + 3] = (color >> 24);
+// 	}
+// }
+// 	mlx_put_image_to_window(mlx, win, image, 0, 0);
+//     mlx_loop(mlx);
+// }
+// ----------TRIANGLE----------
+int main(void)
 {
-    void *mlx = mlx_init();
-    void *win = mlx_new_window(mlx, 640, 360, "Tutorial Window - Create Image");
-
-    void *image = mlx_new_image(mlx, 640, 360);
-
-    int pixel_bits;
-	int line_bytes;
-	int endian;
-	char *buffer = mlx_get_data_addr(image, &pixel_bits, &line_bytes, &endian);
-
-	int color = 0xABCDEF;
-
-	if (pixel_bits != 32)
-	    color = mlx_get_color_value(mlx, color);
-
-	for(int y = 0; y < 360; ++y)
-	for(int x = 0; x < 640; ++x)
-{
-	int pixel = (y * line_bytes) + (x * 4);
-
-	if (endian == 1)        // Most significant (Alpha) byte first
+	void	  *mlx;
+	void	  *mlx_win;
+	t_data  img;
+	int     i;
+	int     j;
+	int     k;
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 600, 400, "Galochka, ti schas umresh");
+	img.img = mlx_new_image(mlx, 600, 400);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+	for (i = 250, k = 99; i < 300; i++, k -= 2)
 	{
-		buffer[pixel + 0] = (color >> 24);
-		buffer[pixel + 1] = (color >> 16) & 0xFF;
-		buffer[pixel + 2] = (color >> 8) & 0xFF;
-		buffer[pixel + 3] = (color) & 0xFF;
+	  for (j = 150 + k; j < 250; j++)
+	    my_mlx_pixel_put(&img, i, j, 0xABEFCD);
 	}
-	else if (endian == 0)   // Least significant (Blue) byte first
+	for (i = 300, k = 0; i < 350; i++, k += 2)
 	{
-		buffer[pixel + 0] = (color) & 0xFF;
-		buffer[pixel + 1] = (color >> 8) & 0xFF;
-		buffer[pixel + 2] = (color >> 16) & 0xFF;
-		buffer[pixel + 3] = (color >> 24);
+	  for (j = 150 + k; j < 250; j++)
+	    my_mlx_pixel_put(&img, i, j, 0xABEFCD);
 	}
-}
-	mlx_put_image_to_window(mlx, win, image, 0, 0);
-    mlx_loop(mlx);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	mlx_loop(mlx);
+	return (0);
 }
