32562591dc9b4c36acc2d4f08294fb746a3c7cbb
lavrenovamaria <wrickard@student.21-school.ru>
2021-09-27 17:35:33 +0300

    mandelbrot is available now

--- srcs/main.c
+++ srcs/main.c
@@ -6,43 +6,129 @@ int	encode_rgb(uint8_t red, uint8_t green, uint8_t blue)
 	return (red << 16 | green << 8 | blue);
 }
 
-int	handle_keypress(int keysym, t_img *img)
+t_complex	init_complex(double re, double im)
 {
-	if (keysym == XK_Escape)
-		mlx_destroy_window(img->mlx_ptr, img->win_ptr);
-	return (0);
+	t_complex	complex;
+
+	complex.re = re;
+	complex.im = im;
+	return (complex);
 }
 
-int	handle_keyrelease(int keysym, void *img)
+t_complex complex_square(t_complex num)
 {
-	return (0);
+	t_complex result;
+	result.re = num.re * num.re - num.im * num.im;
+	result.im = 2 * num.re * num.im;
+	return result;
 }
 
-void	my_mlx_pixel_put(t_img *img, int x, int y, int color)
+double complex_module(t_complex num)
+{
+	double result;
+	result = sqrt(num.re * num.re + num.im * num.im);
+	return result;
+}
+
+t_complex complex_sum(t_complex num1, t_complex num2)
+{
+	t_complex result;
+	result.re = num1.re + num2.re;
+	result.im = num1.im + num2.im;
+	return result;
+}
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
 	char	*dst;
 
-	dst = img->addr + (y * img->line_length + x * (img->bits_per_pixel / 8));
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
 	*(unsigned int*)dst = color;
 }
+// int	handle_keypress(int keysym, t_fractal *fractal)
+// {
+// 	if (keysym == XK_Escape)
+// 		mlx_destroy_window(fractal->mlx_ptr, fractal->win_ptr);
+// 	return (0);
+// }
+
+// int	handle_keyrelease(int keysym, void *fractal)
+// {
+// 	return (0);
+// }
+int mandelbrot(t_fractal *fractal)
+{
+	t_complex	z;
+	t_complex	c;
+	int color = 0xFFFFFF;
+	double		x = 0;
+	double		y = 0;
+	double		a = 0;
+	double		b = 0;
+	int			iter = 50;
+	int calibrate = 100;
+
+	while(x < 600)
+	{
+		while(y < 600)
+		{
+			b = (x - 600/2)/calibrate;
+			a = (600/2 - y)/calibrate;
+			z.im = 0;
+			z.re = 0;
+			c.re = b;
+			c.im = a;
+			while(iter--)
+			{
+				z = complex_square(z);
+				z = complex_sum(z, c);
+				if (complex_module(z) > 2)
+					break ;
+			}
+			if (iter <= 0)
+			{
+				my_mlx_pixel_put(fractal->img, x, y, 0xFFFFFF);
+			}
+			else
+			{
+				my_mlx_pixel_put(fractal->img, x, y, 0xFE3D43);
+			}
+			iter = 50;
+			y++;
+		}
+		y = 0;
+		x++;
+	}
+	mlx_put_image_to_window(fractal->mlx_ptr, fractal->win_ptr, fractal->img->img, 0, 0);
+	return (0);
+}
+
+
 
 int	main(void)
 {
-	t_img	img;
+	t_fractal	fractal;
+	t_data		img;
 
-	img.mlx_ptr = mlx_init();
-	if (img.mlx_ptr == NULL)
-		return (MLX_ERROR);
-	img.win_ptr = mlx_new_window(img.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT, "mlx_test");
-	if (img.win_ptr == NULL)
-	{
-		free(img.win_ptr);
-		return (MLX_ERROR);
-	}
-	mlx_loop_hook(img.mlx_ptr, &my_mlx_pixel_put, &img);
-	mlx_hook(img.win_ptr, WHEEL, MASK_WHEEL, &wheel_hook, &img);
-	mlx_hook(img.win_ptr, KEY_PRESS, MASK_KEY_PRESS, &keyboard_hook, &img);
-	mlx_loop(img.mlx_ptr);
-	mlx_destroy_display(img.mlx_ptr);
-	free(img.mlx_ptr);
+	fractal.calibr = 100;
+
+	fractal.mlx_ptr = mlx_init();
+	fractal.win_ptr = mlx_new_window(fractal.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT, "mlx_test");
+	img.img = mlx_new_image(fractal.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+	fractal.img = &img;
+	// if (fractal.mlx_ptr == NULL)
+	// 	return (MLX_ERROR);
+	// if (fractal.win_ptr == NULL)
+	// {
+	// 	free(fractal.win_ptr);
+	// 	return (MLX_ERROR);
+	// }
+	//mlx_hook(fractal.mlx_ptr, &my_mlx_pixel_put, &fractal);
+	// mlx_hook(fractal.win_ptr, WHEEL, MASK_WHEEL, &wheel_hook, &fractal);
+	// mlx_hook(fractal.win_ptr, KEY_PRESS, MASK_KEY_PRESS, &keyboard_hook, &fractal);
+	mandelbrot(&fractal);
+	mlx_loop(fractal.mlx_ptr);
+	//mlx_destroy_display(fractal.mlx_ptr);
+	//free(fractal.mlx_ptr);
 }
